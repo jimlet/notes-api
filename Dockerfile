23cf650612c2104faf932e91795e4248e5e2a88b
jimlet <jim@coutu.org>
2018-05-08 02:01:54 -0500

    dockerized

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:8.11.1-alpine
+
+ADD package.json /tmp/package.json
+RUN cd /tmp && npm install 
+RUN mkdir -p /opt/app && cp -a /tmp/node_modules /opt/app/
+
+WORKDIR /opt/app
+ADD . /opt/app
+
+EXPOSE 8000
+
+CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const router = require('./router');
+const router = require('./app/router');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
